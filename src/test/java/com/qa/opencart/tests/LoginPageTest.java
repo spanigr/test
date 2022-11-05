ced6a508caaa69270a68b796a57633b03cfa4059
Shibani Panigrahi <cvanipanigrahi@gmail.com>
2022-11-05 14:17:56 +0530

    added test3 method

--- src/test/java/com/qa/opencart/tests/LoginPageTest.java
+++ src/test/java/com/qa/opencart/tests/LoginPageTest.java
@@ -72,4 +72,10 @@ public class LoginPageTest extends BaseTest {
 		loginPage.doLoginWrongData(un, pwd);
 	}
 
+	@Test(priority = 0, dataProvider = "loginNegativeData", enabled = false)
+	public void loginNegativeTest3(String un, String pwd,String num) {
+		loginPage.doLoginWrongData(un, pwd);
+	}
+
+
 }
